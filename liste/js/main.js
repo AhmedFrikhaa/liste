9aff4b5ef5f76d163188d79e004a1be8e8cdb494
Ahmed_Frikha <94226059+AhmedFrikhaa@users.noreply.github.com>
2022-03-01 19:01:48 +0100

    first

---
+++ liste/js/main.js
@@ -0,0 +1,14 @@
+const lis=document.querySelectorAll("ol li")
+
+for(let i=0;i<lis.length;i++)
+{
+    lis[i].addEventListener(
+        "click",
+        ()=>{
+            let x=Math.floor(Math.random()*255);
+            let y=Math.floor(Math.random()*255);
+            let z=Math.floor(Math.random()*255);
+            lis[i].style["color"]="rgb("+x+","+y+","+z+")";
+        }
+    )
+}
